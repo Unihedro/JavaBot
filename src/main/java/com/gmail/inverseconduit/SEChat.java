108a4da41f92ccef9d3ffcb3ae5e435ccc6ce0cd
Vincentyfication <vincentyification@gmail.com>
2014-10-20 02:06:29 +0800

    Created project!

---
+++ src/main/java/com/gmail/inverseconduit/SEChat.java
@@ -0,0 +1,35 @@
+// CLASS CREATED 2014/10/19 AT 5:16:59 P.M.
+// SEChat.java by Unihedron
+package com.gmail.inverseconduit;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Generates locations to the destinated address.<br>
+ * SEChat @ com.gmail.inverseconduit
+ * 
+ * @author Unihedron<<a href="mailto:vincentyification@gmail.com"
+ *         >vincentyification@gmail.com</a>>
+ */
+enum SEChat {
+    chatSO("stackoverflow"),
+    chatSE("stackexchange"),
+    chatMSE("meta." + chatSE.dir);
+
+    private final String dir;
+
+    SEChat(String dir) {
+        this.dir = dir;
+    }
+
+    URI urlToRoom(int id) throws IllegalArgumentException {
+        if (id <= 0)
+            throw new IllegalArgumentException("id must be a positive number.");
+        try {
+            return new URI("http://chat." + dir + ".com/rooms/" + id);
+        } catch(URISyntaxException e) {
+            throw new IllegalArgumentException(e.getClass().getName() + ":" + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/gmail/inverseconduit/SimpleConnectionManager.java
@@ -0,0 +1,34 @@
+// CLASS CREATED 2014/10/19 AT 4:48:27 P.M.
+// SimpleConnectionManager.java by Unihedron
+package com.gmail.inverseconduit;
+
+import java.io.IOException;
+
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+
+class SimpleConnectionManager implements ConnectionManager {
+
+    private static final String email = "10395287@opayq.com", password = "Polyhedron0";
+
+    @Override
+    public void establishConnection() {
+        login();
+        try {
+            System.out.println(Request.Post(SEChat.chatSO.urlToRoom(139) + "/java").bodyForm(Form.form().add("input", "~ Hello World").build()).execute().returnContent());
+        } catch(IllegalArgumentException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean login() {
+        try {
+            System.out.println(Request.Post("http://stackoverflow.com/users/login?returnurl=http%3a%2f%2fchat.stackoverflow.com%2frooms%2f139").bodyForm(Form.form().add("email", email).add("password", password).build()).execute().returnContent());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+}
