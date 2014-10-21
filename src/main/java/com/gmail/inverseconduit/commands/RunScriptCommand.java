b8767e4ff4bc1fb699dd940dee61cdea06ad1024
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-21 09:34:36 -0400

    Put userIds in a HashSet.

--- src/main/java/com/gmail/inverseconduit/commands/RunScriptCommand.java
+++ src/main/java/com/gmail/inverseconduit/commands/RunScriptCommand.java
@@ -1,24 +1,40 @@
 package com.gmail.inverseconduit.commands;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import com.gmail.inverseconduit.BotConfig;
 import com.gmail.inverseconduit.JavaBot;
 import com.gmail.inverseconduit.chat.ChatMessage;
 import com.gmail.inverseconduit.chat.ChatMessageListener;
-import org.apache.commons.lang3.StringEscapeUtils;
 
 public class RunScriptCommand implements ChatMessageListener{
+	private final Set<Integer> userIds = new HashSet<Integer>();
+	{
+		userIds.add(3622940);
+		userIds.add(2272617);
+	}
+	
     @Override
     public void onMessage(JavaBot bot, ChatMessage msg) {
         System.out.println("Entered onMessage for RunScriptCommand");
-        if(msg.getUserId() == 3622940 || msg.getUserId() == 2272617) {
-            String m = msg.getMessage();
-            if (msg.getMessage().startsWith(BotConfig.TRIGGER + "eval:")) {
-                String script = StringEscapeUtils.unescapeHtml4(m.substring(m.indexOf(":") + 1));
-                System.out.println("Evaluating script: " + script);
-                Object result = bot.getGroovyShell().evaluate(script);
-                System.out.println(result);
-                bot.sendMessage(msg.getSite(), msg.getRoomId(), result.toString());
-            }
+        if (!userIds.contains(msg.getUserId())){
+        	//ignore message
+        	return;
         }
+        
+        String m = msg.getMessage();
+        if (!msg.getMessage().startsWith(BotConfig.TRIGGER + "eval:")) {
+        	//not a bot command
+        	return;
+        }
+
+        String script = StringEscapeUtils.unescapeHtml4(m.substring(m.indexOf(":") + 1));
+        System.out.println("Evaluating script: " + script);
+        Object result = bot.getGroovyShell().evaluate(script);
+        System.out.println(result);
+        bot.sendMessage(msg.getSite(), msg.getRoomId(), result.toString());
     }
 }
