f007a641365846c8a1d0b3dc7ba0c0538ee5ebc1
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-21 09:41:24 -0400

    Nothing throws that exception.

--- src/main/java/com/gmail/inverseconduit/Main.java
+++ src/main/java/com/gmail/inverseconduit/Main.java
@@ -5,15 +5,11 @@ import com.gmail.inverseconduit.commands.RunScriptCommand;
 public class Main {
     private static JavaBot javaBot;
     public static void main(String[] args) {
-        try {
-            javaBot = new JavaBot();
-            javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
-            // Register command listeners
-            javaBot.addListener(new RunScriptCommand());
+        javaBot = new JavaBot();
+        javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
+        // Register command listeners
+        javaBot.addListener(new RunScriptCommand());
 
-            javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
-        } catch(IllegalStateException ex) {
-            ex.printStackTrace();
-        }
+        javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
     }
 }
