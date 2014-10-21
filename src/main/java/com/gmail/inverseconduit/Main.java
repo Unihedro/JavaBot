3e36dcbba2f870d4f7191d290653a0578dddc271
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-21 09:41:57 -0400

    No need to make "javaBot" a static field.

--- src/main/java/com/gmail/inverseconduit/Main.java
+++ src/main/java/com/gmail/inverseconduit/Main.java
@@ -3,9 +3,8 @@ package com.gmail.inverseconduit;
 import com.gmail.inverseconduit.commands.RunScriptCommand;
 
 public class Main {
-    private static JavaBot javaBot;
     public static void main(String[] args) {
-        javaBot = new JavaBot();
+    	JavaBot javaBot = new JavaBot();
         javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
         // Register command listeners
         javaBot.addListener(new RunScriptCommand());
