0224e0fe8af2e000c2b5c80b94d6ae7fd3e109ad
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-25 09:19:17 -0400

    Merge of 8a73d74: Cherry pick of ff52821

--- src/main/java/com/gmail/inverseconduit/Main.java
+++ src/main/java/com/gmail/inverseconduit/Main.java
@@ -10,28 +10,20 @@ import java.security.Policy;
 public class Main {
     private static JavaBot javaBot;
     public static void main(String[] args) {
-        try {
-            Policy.setPolicy(ScriptSecurityPolicy.getInstance());
-            System.setSecurityManager(ScriptSecurityManager.getInstance());
+        Policy.setPolicy(ScriptSecurityPolicy.getInstance());
+        System.setSecurityManager(ScriptSecurityManager.getInstance());
 
-            javaBot = new JavaBot();
-            boolean loggedIn = javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
-            if (!loggedIn) {
-                System.out.println("Login failed!");
-                return;
-            }
-            // Register command listeners
-            javaBot.addListener(new RunScriptCommand());
+        javaBot = new JavaBot();
+        boolean loggedIn = javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
+        if (!loggedIn) {
+            System.out.println("Login failed!");
+            return;
+        }
+        javaBot.addListener(new RunScriptCommand());
+        javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
 
-            // Join chat
-            javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
-
-            // Begin processing
-            while(true) {
-                javaBot.processMessages();
-            }
-        } catch(IllegalStateException ex) {
-            ex.printStackTrace();
+        while(true) {
+            javaBot.processMessages();
         }
     }
 }
