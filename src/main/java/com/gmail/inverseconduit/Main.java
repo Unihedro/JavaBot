9da7ca712a0b34675ae72eb54db341d005480a3a
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-25 09:20:19 -0400

    Merge of 8a73d74: Cherry pick of 64d33f1

--- src/main/java/com/gmail/inverseconduit/Main.java
+++ src/main/java/com/gmail/inverseconduit/Main.java
@@ -8,17 +8,17 @@ import com.gmail.inverseconduit.security.ScriptSecurityPolicy;
 import java.security.Policy;
 
 public class Main {
-    private static JavaBot javaBot;
     public static void main(String[] args) {
         Policy.setPolicy(ScriptSecurityPolicy.getInstance());
         System.setSecurityManager(ScriptSecurityManager.getInstance());
 
-        javaBot = new JavaBot();
+        JavaBot javaBot = new JavaBot();
         boolean loggedIn = javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
         if (!loggedIn) {
             System.out.println("Login failed!");
             return;
         }
+        
         javaBot.addListener(new RunScriptCommand());
         javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
 
