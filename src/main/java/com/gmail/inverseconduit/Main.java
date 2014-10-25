ff528216b5fd26d441b8a90d17ffe310a4d60391
Michael Angstadt <mike.angstadt@gmail.com>
2014-10-25 09:19:17 -0400

    Exception isn't thrown by anything.

--- src/main/java/com/gmail/inverseconduit/Main.java
+++ src/main/java/com/gmail/inverseconduit/Main.java
@@ -1,6 +1,5 @@
 package com.gmail.inverseconduit;
 
-import com.gmail.inverseconduit.bot.BotConfig;
 import com.gmail.inverseconduit.bot.JavaBot;
 import com.gmail.inverseconduit.commands.RunScriptCommand;
 import com.gmail.inverseconduit.security.ScriptSecurityManager;
@@ -11,24 +10,20 @@ import java.security.Policy;
 public class Main {
     private static JavaBot javaBot;
     public static void main(String[] args) {
-        try {
-            Policy.setPolicy(ScriptSecurityPolicy.getInstance());
-            System.setSecurityManager(ScriptSecurityManager.getInstance());
+        Policy.setPolicy(ScriptSecurityPolicy.getInstance());
+        System.setSecurityManager(ScriptSecurityManager.getInstance());
 
-            javaBot = new JavaBot();
-            boolean loggedIn = javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
-            if (!loggedIn) {
-                System.out.println("Login failed!");
-                return;
-            }
-            javaBot.addListener(new RunScriptCommand());
-            javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
+        javaBot = new JavaBot();
+        boolean loggedIn = javaBot.login(SESite.STACK_OVERFLOW, BotConfig.LOGIN_EMAIL, BotConfig.PASSWORD);
+        if (!loggedIn) {
+            System.out.println("Login failed!");
+            return;
+        }
+        javaBot.addListener(new RunScriptCommand());
+        javaBot.joinChat(SESite.STACK_OVERFLOW, 1);
 
-            while(true) {
-                javaBot.processMessages();
-            }
-        } catch(IllegalStateException ex) {
-            ex.printStackTrace();
+        while(true) {
+            javaBot.processMessages();
         }
     }
 }
